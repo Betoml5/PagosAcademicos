62d258fa9b2c18c4eb34be663f31b8b6ab0d7bcf
Betoml5 <betoml5@hotmail.com>
2023-11-11 17:14:55 -0600

    Agregue controlador para pagos en el area admin

--- PagosAcademicos/Areas/Admin/Controllers/PagosController.cs
+++ PagosAcademicos/Areas/Admin/Controllers/PagosController.cs
@@ -1,12 +1,40 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PagosAcademicos.Areas.Admin.Models.ViewModels;
+using PagosAcademicos.Models.Entities;
+using PagosAcademicos.Repositories;
 
 namespace PagosAcademicos.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class PagosController : Controller
     {
+
+        private readonly Repository<Pago> ctx;
+        public PagosController(Repository<Pago> ctx)
+        {
+            this.ctx = ctx;
+        }
         public IActionResult Index()
         {
-            return View();
+
+            var vm = ctx
+                .GetAll()
+                .OrderBy(x => x.Fecha)
+                .Select(pago => new IndexPagosViewModel()
+            {
+                Id = pago.Id,
+                Usuario = new UsuarioModel()
+                {
+                    Id = pago.Usuario.Id,
+                    Nombre = pago.Usuario.Nombre
+                },
+                Monto = pago.Monto,
+                Concepto = pago.Concepto,
+                Fecha = pago.Fecha
+                
+            });
+
+            return View(vm);
         }
     }
 }
---
+++ PagosAcademicos/Areas/Admin/Models/ViewModels/IndexPagosViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace PagosAcademicos.Areas.Admin.Models.ViewModels
+{
+    public class IndexPagosViewModel
+    {
+        public int Id { get; set; }
+        public UsuarioModel Usuario { get; set; } = null!;
+
+        public decimal Monto { get; set; }
+
+        public string Concepto { get; set; } = null!;
+        public DateTime Fecha { get; set; } 
+
+    }
+
+    public class UsuarioModel
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = null!;
+    }
+}
