31874bb636f752e32fafd5677ccdc8b74657d52c
Betoml5 <betoml5@hotmail.com>
2023-12-07 23:16:32 -0600

    Agregue validacion de nulls

--- PagosAcademicos/Areas/Admin/Controllers/HomeController.cs
+++ PagosAcademicos/Areas/Admin/Controllers/HomeController.cs
@@ -22,14 +22,30 @@ namespace PagosAcademicos.Areas.Admin.Controllers
 		public IActionResult Index()
 		{
 			var vm = new IndexAdminViewModel();
-			var pagos = pagosRepository.GetAll().OrderBy(x => x.Fecha);
-			var pagoMasAlto = pagosRepository.GetAll().OrderByDescending(x => x.Monto).FirstOrDefault();
-			var ultimoPago = pagosRepository.GetAll().OrderByDescending(x => x.Fecha).FirstOrDefault();
+
+			var pagos = pagosRepository
+				.GetAll()
+				.OrderBy(x => x.Fecha);
+
+			if (!pagos.Any())
+			{
+				return View(vm);
+			}
+
+			var pagoMasAlto = pagosRepository
+				.GetAll()
+				.OrderByDescending(x => x.Monto)
+				.FirstOrDefault();
+			var ultimoPago = pagosRepository
+				.GetAll()
+				.OrderByDescending(x => x.Fecha)
+				.FirstOrDefault();
+
 
 			vm.Pagos = pagos;
 			vm.FechaUltimoPago = ultimoPago.Fecha;
 			vm.UltimoPago = ultimoPago.Monto;
-			vm.MontoMasAlto = pagoMasAlto.Monto ;
+			vm.MontoMasAlto = pagoMasAlto.Monto;
 			vm.NumeroPagos = pagos.Count();
 
 			return View(vm);
--- PagosAcademicos/Areas/Admin/Models/ViewModels/IndexAdminViewModel.cs
+++ PagosAcademicos/Areas/Admin/Models/ViewModels/IndexAdminViewModel.cs
@@ -5,9 +5,9 @@ using PagosAcademicos.Models.ViewModels;
 namespace PagosAcademicos.Areas.Admin.Models.ViewModels;
 public class IndexAdminViewModel
 {
-    public decimal MontoMasAlto { get; set; }
-    public decimal UltimoPago { get; set; }
-    public int NumeroPagos { get; set; }
+    public decimal MontoMasAlto { get; set; } = 0;
+    public decimal UltimoPago { get; set; } = 0;
+    public int NumeroPagos { get; set; } = 0;
     public DateTime FechaUltimoPago { get; set; }
 
     public IEnumerable<Pago>? Pagos { get; set; } = null!;
